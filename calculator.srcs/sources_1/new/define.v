d563fecfe2a94e508ee43c16f7a2d0ab2a5dfc9b
JuliusChoy <choyjulius@gmail.com>
2025-12-12 20:27:39 +0800

    Create define.v

---
+++ calculator.srcs/sources_1/new/define.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.12.2025 21:29:25
+// Design Name: 
+// Module Name: define
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`define HALT  4'b0000
+`define PUSHI 4'b0001
+`define PUSH  4'b0010
+`define POP   4'b0011
+`define JMP   4'b0100
+`define JZ    4'b0101
+`define JNZ   4'b0110
+`define IN    4'b1101
+`define OUT   4'b1110
+`define OP    4'b1111
+
+`define DATA_WIDTH 5'd31
